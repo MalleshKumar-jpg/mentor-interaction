dda3131742267f4f559ee3b0c1767ca10b6a65cd
MalleshKumar-jpg <kmkumar0112@gmail.com>
2025-03-09 21:49:51 +0530

    fixed: chosen mentee appears in the sidebar

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     const url_params= new URLSearchParams(window.location.search);
-    const role = url_params.get('role');
-
+    const role = url_params.get('role'); /* retrieving the type of user (mentor/mentee) from url*/
+    
     if (document.body.id==='login-page'){
         document.getElementById('role').value = role;
         document.getElementById('login-form').addEventListener('submit', function (event){
@@ -11,7 +11,7 @@ window.onload = function() {
             event.preventDefault();
             if  (role==='mentor'){
                 window.location.href = 'mentor_menteeslist.html';
-            } else if (role === 'mentee'){
+            } else if (role==='mentee'){
                 window.location.href = 'main.html';
             }
         }else{
@@ -19,18 +19,37 @@ window.onload = function() {
         }
         });
     }
-    
+
+    if (document.body.id=="menteeslist-page"){
+        const mentee_cards=document.querySelectorAll('.mentee-card');
+
+        mentee_cards.forEach(card => {
+            card.addEventListener('click',function(){
+                const chosenMentee=card.getAttribute('data-mentee');
+                sessionStorage.setItem('chosen_mentee',chosenMentee)
+            });
+        });
+    }
+
+    /* hiding buttons and elements based on the type of user */
     if (document.body.id === 'main-page'){
         const storedRole=sessionStorage.getItem('role');
 
         if (storedRole == 'mentor'){
-            document.querySelectorAll('.mentee-only-btn').forEach(btn => btn.style.display='none');
-        }else if(storedRole == 'mentee'){
-            document.querySelectorAll('.mentor-only-btn, .mentor-only-container').forEach(elem => elem.style.display='none');
+            document.querySelectorAll('.mentee-only-btn').forEach(btn => btn.style.display='none'); /* hiding the class of mentee-only-btn */
+            const sidebar_mentee_name=document.getElementById('mentee-name');
+            sidebar_mentee_name.textContent=sessionStorage.getItem('chosen_mentee');
             
-            const sidebar_mentee_name = document.getElementById('mentee-name');
+        }else if(storedRole == 'mentee'){
+            document.querySelectorAll('.mentor-only-btn, .mentor-only-container').forEach(elem => elem.style.display='none'); /* hiding the class of mentor-only-btn , mentor-only container */
+            
+            const sidebar_mentee_name = document.getElementById('mentee-name'); /* hiding the sidebar mentee-name . we cant include this in above code , because mentee-name is an id*/
             sidebar_mentee_name.style.backgroundColor='darkgreen';
             sidebar_mentee_name.textContent='';
+
+            const header_role=document.getElementById('user-role');
+            header_role.textContent='MENTEE';
         }
+        
     }
 };
